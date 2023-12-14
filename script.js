080266d149e3eb5c612022dbc3a5825fb59275cf
Anik Dutta <121732854+anikmathlibrary@users.noreply.github.com>
2023-12-15 01:58:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Starting of Action Dots
 let slideIndex = 0;
 showSlides();
 
@@ -31,3 +32,23 @@ function currentSlide(n) {
     slideIndex = n - 1;
     showSlides();
 }
+//Ending of Action Dots
+
+//Starting of Action Top Toggle
+window.onscroll = function() {
+    toggleToTopButton();
+};
+function toggleToTopButton() {
+    var button = document.getElementById('toTopButton');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = 'block';
+    }
+    else {
+        button.style.display = 'none';
+    }
+}
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+//Ending of Action Top Toggle
