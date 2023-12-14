89a3b9badfb0642de0dc69200081a40f4b116a17
Anik Dutta <121732854+anikmathlibrary@users.noreply.github.com>
2023-12-15 01:32:20 +0530

    Update script-eco-education.js

--- webpages/script-eco-education.js
+++ webpages/script-eco-education.js
@@ -4,3 +4,23 @@ function redirectToEnroll() {
 function redirectToDonate() {
     window.location.href='#donate-id';
 }
+
+
+//Starting of Action Top Toggle
+window.onscroll = function() {
+    toggleToTopButton();
+};
+function toggleToTopButton() {
+    var button = document.getElementById('toTopButton');
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = 'block';
+    }
+    else {
+        button.style.display = 'none';
+    }
+}
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+//Ending of Action Top Toggle
